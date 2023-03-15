db73e582f3a939cd7451b53a59f73de932d2367f
guilherme <guilhermesantana@saovicente.sp.gov.br>
2023-03-15 13:45:36 -0300

    IMPROVEMENT - mudanca da cor do range

--- src/app/pages/admin/admin-servicos-detalhes/admin-servicos-detalhes.component.ts
+++ src/app/pages/admin/admin-servicos-detalhes/admin-servicos-detalhes.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-servicos-detalhes',
@@ -9,16 +9,47 @@ import { Component, OnInit } from '@angular/core';
 export class AdminServicosDetalhesComponent implements OnInit {
 
   servico: any
+  @Input() percent!: number;
+
+  colorOuter : any
+  colorInner : any
 
   constructor
-  ()
+  (
+    private elementRef: ElementRef
+  )
   {
     this.servico = history.state.data
 
     console.log(this.servico)
+
+    this.percent = this.servico.percentual
+
+    console.log(this.percent)
   }
 
   ngOnInit(): void {
+
+    this.verifyColor()
+  }
+
+  verifyColor()
+  {
+    if(this.percent < 50)
+    {
+      this.colorInner = 'rgb(255, 79, 79)'
+      this.colorOuter = 'red'
+    }
+    else if(this.percent > 50 && this.percent < 100)
+    {
+      this.colorInner = 'rgb(255, 146, 122)'
+      this.colorOuter = 'rgb(255, 126, 97)'
+    }
+    else
+    {
+      this.colorInner = '#C7E596'
+      this.colorOuter = '#78C000'
+    }
   }
 
   downloadPdf(pdfUrl : string) {
